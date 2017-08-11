fc7b9253a5bbcd20ecd42301c442ba9440ff7fa8
Kyle Pollich <kpollich1@gmail.com>
2017-08-10 22:32:02 -0400

    fixup! Allow channel name to be figured.

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('dotenv').config({ silent: true })
 
 const Bot = require('./bot')
 
-console.log(process.env.ALERT_CHANNEL)
-
 const bot = new Bot({
   token: process.env.SLACK_API_TOKEN,
   name: 'Street Cleaning Bot',
